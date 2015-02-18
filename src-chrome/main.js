246c5d070e80e3cd03b56dae58c7c3ca8bdbf8dc
Mauricio Kruijer <mauricio@leadsocial.com>
2015-02-18 23:21:50 +0100

    Updated Tower icon and clone over SSH
    Changes default behaviour; clones over SSH instead of HTTPS. No release packages yet

--- src-chrome/main.js
+++ src-chrome/main.js
@@ -2,6 +2,6 @@
     var buttonElement = document.querySelector("a[data-url^='github-mac://']");
 
     if ( buttonElement != null ) {
-        buttonElement.href = buttonElement.getAttribute("data-url");
+        buttonElement.href = buttonElement.getAttribute("data-url").replace('https:\/\/github.com/', 'git@github.com:');
     }
 }());
